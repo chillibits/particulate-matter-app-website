9749ef2db145a0e550615e840d5cfa30a27d3216
Marc Auberer <marc.auberer@chillibits.com>
2020-01-19 03:31:50 +0100

    Add Google Play trademark clause

---
+++ src/contact/contact_process.php
@@ -0,0 +1,20 @@
+<?php
+    $to = "info@chillibits.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $message = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: $from\r\n";
+	$headers .= "Reply-To: $from\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+	$body = "Nachricht von $name ($from)<br>";
+    $body .= "Betreff: $subject<br>";
+    $body .= "Message: $message<br>";
+
+    $send = mail($to, "Neue Nachricht von ChilliBits Homepage", $body, $headers);
+    header("Location: ./index.php");
+?>
